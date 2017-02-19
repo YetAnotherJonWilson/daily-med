b33206764aaae706fd66e7e5851d14c60b1e6c36
Jon Wilson <jonwilsoncomedy@gmail.com>
2017-02-19 17:48:21 -0600

    adding props and state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react'
-import './App.css'
+import Header from './Header'
 
 const App = () => {
-    return (<div className="App">Header Here</div>)
+    return (
+        <div className="App">
+            <Header name="here"/>
+            <Header name="there"/>
+        </div>
+    )
 }
 
 export default App
---
+++ src/Header.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import './Header.css'
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {type: 'Header'};
+        this.frenchify = this.frenchify.bind(this)
+    }
+
+    frenchify() {
+        this.setState({type: 'La tete'})
+    }
+
+    render() {
+    return (
+        <div className="Header">
+            {this.state.type} {this.props.name}
+            <br/>
+            <button onClick={this.frenchify}>Frenchify</button>
+        </div>
+    )
+}}
+
+export default Header
